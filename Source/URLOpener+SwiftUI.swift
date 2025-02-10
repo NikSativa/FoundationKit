c8c5148f02b83ff2baa53aaa10d89ccbaf613092
Mykyta Konopelko <nik.sativa@gmail.com>
2025-02-10 16:16:57 +0100

    fix access URLOpener+SwiftUI

--- Source/URLOpener+SwiftUI.swift
+++ Source/URLOpener+SwiftUI.swift
@@ -5,7 +5,7 @@ import SwiftUI
 #if swift(>=6.0)
 @MainActor
 @available(iOS 15.0, tvOS 15.0, macOS 12.0, macCatalyst 15.0, watchOS 8.0, *)
-protocol URLOpening: Sendable {
+public protocol URLOpening: Sendable {
     #if os(watchOS)
     func route(to url: URL)
     #else
@@ -16,18 +16,22 @@ protocol URLOpening: Sendable {
 
 @MainActor
 @available(iOS 15.0, tvOS 15.0, macOS 12.0, macCatalyst 15.0, watchOS 8.0, *)
-final class URLOpener: Sendable {
+public struct URLOpener: Sendable {
     private let openURLAction: OpenURLAction
 
-    init() {
+    public init() {
         self.openURLAction = .init { url in
             return .systemAction(url)
         }
     }
+
+    public init(openURLAction: OpenURLAction) {
+        self.openURLAction = openURLAction
+    }
 }
 #else
 @available(iOS 15.0, tvOS 15.0, macOS 12.0, macCatalyst 15.0, watchOS 8.0, *)
-protocol URLOpening {
+public protocol URLOpening {
     #if os(watchOS)
     func route(to url: URL)
     #else
@@ -37,13 +41,17 @@ protocol URLOpening {
 }
 
 @available(iOS 15.0, tvOS 15.0, macOS 12.0, macCatalyst 15.0, watchOS 8.0, *)
-final class URLOpener {
+public struct URLOpener {
     private let openURLAction: OpenURLAction
 
-    init() {
+    public init() {
         self.openURLAction = .init { url in
             return .systemAction(url)
         }
+    }
+
+    public init(openURLAction: OpenURLAction) {
+        self.openURLAction = openURLAction
     }
 }
 #endif
@@ -51,13 +59,22 @@ final class URLOpener {
 @available(iOS 15.0, tvOS 15.0, macOS 12.0, macCatalyst 15.0, watchOS 8.0, *)
 extension URLOpener: URLOpening {
     #if os(watchOS)
-    func route(to url: URL) {
+    public func route(to url: URL) {
         openURLAction.callAsFunction(url)
     }
     #else
-    func route(to url: URL, completion: @escaping Completion) {
+    public func route(to url: URL, completion: @escaping Completion) {
         openURLAction.callAsFunction(url, completion: completion)
     }
     #endif
+}
+#endif
+
+#if !os(watchOS)
+@available(iOS 15.0, tvOS 15.0, macOS 12.0, macCatalyst 15.0, watchOS 8.0, *)
+public extension URLOpening {
+    func route(to url: URL) {
+        route(to: url, completion: { _ in })
+    }
 }
 #endif
